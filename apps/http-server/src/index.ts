7b27042d1bbe96b225606dba22178dd5aa3718e6
Abhishek Kumar Roy <145221033+royabhishek2003@users.noreply.github.com>
2026-06-21 03:01:28 +0530

    Update response message from 'Hello World!' to 'Hello World!!!'

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -10,7 +10,7 @@ app.use(express.json());
 const PORT = 3001;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World!!!");
 });
 
 app.post("/signup", async (req, res) => {
